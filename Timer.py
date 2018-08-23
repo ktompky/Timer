f4c8b601b0c97c1232f992aad3774061c7ffffbb
Kelly Tompkins <k.tompkins005@gmail.com>
2018-08-23 10:05:36 -0500

    Update and rename Timer.java to Timer.py

---
+++ Timer.py
@@ -0,0 +1,9 @@
+import time
+
+seconds = int(input("How many seconds to wait?"))
+
+for i in range(seconds):
+    print(str(seconds - i) + " seconds remain")
+    time.sleep(1)
+
+print('Time is up.')
